ea2f9a999a4a6a1f02c510db28c37152f45ba74f
dianacamacho <dcam1901@yahoo.com>
2017-03-19 16:36:18 -0500

    more require with json

--- D10-More-On-Require/greet/german.js
+++ D10-More-On-Require/greet/german.js
@@ -1,5 +1,8 @@
+// with json files, require returns the object created from the json text, no need for modules.export
+var greetings = require('./greetings.json');
+
 var greet = function() {
-  console.log("Hallo!");
+  console.log(greetings.de);
 };
 
 module.exports = greet;
